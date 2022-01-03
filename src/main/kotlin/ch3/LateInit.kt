40831fdbe721fb8bdee07fd66e3efab8f1feabfd
Davit Mumladze <dmuml10@gmail.com>
2022-01-03 22:01:36 +0200

    Add Operator override and lazy evaluation

---
+++ src/main/kotlin/ch3/LateInit.kt
@@ -0,0 +1,15 @@
+package ch3
+
+class LateInitDemo {
+    lateinit var name: String
+
+    fun initializeName() {
+        println("Before assignment: ${::name.isInitialized}")
+        name = "World"
+        println("After assignment: ${::name.isInitialized}")
+    }
+}
+
+fun main() {
+    LateInitDemo().initializeName()
+}
---
+++ src/main/kotlin/ch3/Operator.kt
@@ -0,0 +1,11 @@
+package ch3
+
+data class Point(val x: Int, val y: Int)
+
+operator fun Point.unaryMinus() = Point(-x, -y)
+
+val point = Point(10, 20)
+
+fun main() {
+    println(-point)  // prints "Point(x=-10, y=-20)"
+}
