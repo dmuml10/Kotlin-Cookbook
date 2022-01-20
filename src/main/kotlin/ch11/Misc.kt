ec810ce6fff58d57409bf20799d549f3ef4e20dd
Davit Mumladze <dmuml10@gmail.com>
2022-01-20 20:56:12 +0200

    Add When add radix

--- src/main/kotlin/ch11/Misc.kt
+++ src/main/kotlin/ch11/Misc.kt
@@ -1,9 +1,24 @@
 package ch11
 
+fun printMod3(n: Int) {
+    when(n % 3) {
+        0 -> println("0")
+        1 -> println("1")
+        2 -> println("2")
+        //else -> println("Houston we have a problem...")
+    }
+}
+
 fun main() {
     println(KotlinVersion.CURRENT)
 
     repeat(5) {
         println("Counting: $it")
     }
+
+    printMod3(13)
+
+    println("Some String".replace("str", "newStr", true))
+
+    val str = 42.toString(radix = 2)
 }
