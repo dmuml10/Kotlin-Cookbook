ce0a0deff6351a289b16a892d8ea819773af92af
Davit Mumladze <dmuml10@gmail.com>
2022-01-24 21:27:20 +0200

    Add WithCOntext

---
+++ src/main/kotlin/ch13/WithContext.kt
@@ -0,0 +1,25 @@
+package ch13
+
+import kotlinx.coroutines.*
+
+suspend fun retrieve1(url: String) = coroutineScope {
+    async(Dispatchers.IO) {
+        println("Retrieving data on ${Thread.currentThread().name}")
+        delay(100L)
+        "asyncResults"
+    }.await()
+}
+
+suspend fun retrieve2(url: String) =
+    withContext(Dispatchers.IO) {
+        println("Retrieving data on ${Thread.currentThread().name}")
+        delay(100L)
+        "withContextResults"
+    }
+
+fun main() = runBlocking<Unit> {
+    val result1 = retrieve1("www.mysite.com")
+    val result2 = retrieve2("www.mysite.com")
+    println("printing result on ${Thread.currentThread().name} $result1")
+    println("printing result on ${Thread.currentThread().name} $result2")
+}
